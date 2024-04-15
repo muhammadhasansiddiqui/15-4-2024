be5752e3a3a643bb8be0da5dff10b0788cee9bc5
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-04-15 14:56:09 +0500

    work

---
+++ index.js
@@ -0,0 +1,18 @@
+// var city =prompt("Enter your city name");
+// city=city.toLocaleUpperCase();
+
+// if (karachi) {
+//     console.log("Welcome to the city of lights");
+// }else  (lahore) {
+//     console.log("Welcome to the city of lights");
+// // }
+    
+// var city =prompt("Enter your city name");   
+// city=city.toLocaleUpperCase();
+// console.log(city);
+// if (city=="KARACHI") {
+//     console.log("Welcome to the city of lights");
+// }else
+// console.log("Welcome");
+
+
