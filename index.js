ca6730684d67b1484cb67d4e3801d1a9a61e1734
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-04-17 14:42:40 +0500

    work

--- index.js
+++ index.js
@@ -15,4 +15,48 @@
 // }else
 // console.log("Welcome");
 
+//Rounding numbers
 
+
+
+// var firstName = prompt("Enter your first name");
+// var lastName = prompt("Enter your last name");
+// var fullName = firstName + " " + lastName;
+
+// console.log("fullName===>", fullName.toUpperCase());
+
+
+
+//  var newResult = +prompt("Enter your number");
+
+//  console.log("newResult", newResult)
+
+//  var afterRound = Math.round(newResult);
+//  console.log("afterRound",afterRound);
+
+//     var afterFloor = Math.floor(newResult);
+//     console.log("afterFloor", afterFloor);
+
+
+//  var afterceil = Math.ceil(newResult);
+//  console.log("afterceil", afterceil);
+ 
+// var student ="Hasan";
+// var student = "".toUpperCase
+
+// console.log(student.slice(0,4));
+ 
+// var student ="hasan";
+
+// var studentUpper = student.toUpperCase();
+
+// console.log(studentUpper.slice(0,2));
+
+new Date()
+Wed Apr 17 2024 14:17:58 GMT+0500 (Pakistan Standard Time)
+var time = new Date ()
+undefined
+Date.now
+ƒ now() { [native code] }
+Date.now()
+1713345590847
